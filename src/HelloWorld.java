1c4739ef1c59e23c58b3e7c7199d6c9c1cf18a4f
lcj-github <lcjman_34@163.com>
2015-06-28 13:11:45 +0800

    提交到本地 仓库

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author lcj test 20150628
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("world 看过来");
+
+	}
+
+}
