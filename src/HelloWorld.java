c65d2208b3a0a0fa0d55427457d073d9e7f46643
lichunjian <lichunjian@gw.com.cn>
2016-04-26 13:17:53 +0800

    20160426 13:16 公司台式机操作  -----增加 输出  h1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,7 @@
  * @author lcj test 20150628
  * 
  * 20160405 10:37 公司台式机操作
+ * 20160426 13:16 公司台式机操作  -----增加 输出  h1
  *
  */
 public class HelloWorld {
@@ -11,7 +12,9 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("world 鐪嬭繃鏉dit");
+		System.out.println("world dit");
+		
+		System.out.println("h1");  //20160426 13:16 公司台式机操作  -----增加 输出  h1
 
 	}
 
